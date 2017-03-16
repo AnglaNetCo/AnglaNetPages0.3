cc97fd157d931b744791d5be2a622dcd590e4b6e
GovjadkoIlja <govjadkoilja@yandex.ru>
2017-03-16 21:30:41 +0300

    Сделал человеческий хедер и добавил прототип футера. Надо в хедер еще добавить различные иконки, логотип и поработать с футером

---
+++ WebApplication2/WebApplication2/app/footer/footer.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-footer',
+    templateUrl: './app/footer/footer.component.html',
+    styleUrls: ['./app/footer/footer.component.css']
+})
+export class FooterComponent {
+    
+}
---
+++ WebApplication2/WebApplication2/app/header/header.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-header',
+    templateUrl: './app/header/header.component.html',
+    styleUrls: ['./app/header/header.component.css']
+})
+export class HeaderComponent {
+    selected: number;
+
+    constructor() {
+        //this.titles = ["Tasks", "Repetitor"];
+        console.log("Hello");
+    }
+
+    select(n: number) {
+        this.selected = n;
+    }
+}
